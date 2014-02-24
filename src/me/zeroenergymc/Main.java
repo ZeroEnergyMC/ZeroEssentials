6b6d20fc9d1faa42aa0794b1ec28c87660b0c733
ZeroEnergyMC <grechwow@gmail.com>
2014-02-24 20:31:12 +0000

    BookMeta
    Importing book meta.

--- src/me/zeroenergymc/Main.java
+++ src/me/zeroenergymc/Main.java
@@ -15,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.permissions.Permission;
@@ -44,6 +45,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable() {
 		consoleLogger.info(pl + " enabled version: " + ver);
 		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this.PlayerListener, this);
 		manager = new MyConfigManager(this);
 		try
 		{
@@ -126,6 +128,7 @@ public class Main extends JavaPlugin implements Listener {
 		{
 
 			player.setHealth(20D);
+			player.setFoodLevel(20);
 			player.sendMessage(ChatColor.GREEN + "You have been healed!");
 		}
 
@@ -142,9 +145,21 @@ public class Main extends JavaPlugin implements Listener {
 			warp1.setLore(lore);
 			eye.setItemMeta(warp1);
 			powerinv.addItem(eye);
-			
-
 		}
+		
+		if (cmd.getName().equalsIgnoreCase("help")) {
+			if(sender instanceof Player){
+		 		ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+				BookMeta bm = (BookMeta)book.getItemMeta();
+				bm.addPage(ChatColor.GREEN + " == Legacy Miners ==  \n\n" + ChatColor.RED + "Welcome to the server! \n" + ChatColor.GOLD + "Owner: ZeroEnergyMC\nAdmin(s): qasw287\nModerator(s): End51" + ChatColor.BLUE + "\n\nMake sure you vote daily to receive rewards.\n\nBe active and friendly to achieve staff.");
+				bm.setAuthor(ChatColor.RED + "Legacy King");
+				bm.setTitle(ChatColor.GREEN + "== Legacy Miners ==");
+				book.setItemMeta(bm);
+				player.getInventory().addItem(book);
+				consoleLogger.info(player.getName() + " ran the /help command.");
+			}
+		
+	}
 		return false;
 	}
 }
